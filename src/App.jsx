6d64c8cbc3c15720f44e6c7007af68458b346582
Code-harness <codeharness1@gmail.com>
2025-12-07 14:57:47 +0200

    Vercel Analytics Config

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
 import AppRouter from "./AppRouter";
+import { Analytics } from "@vercel/analytics/next";
 
-const App = () => <AppRouter />;
+const App = () => (
+  <>
+    <AppRouter />
+    <Analytics />
+  </>
+);
+
 export default App;
